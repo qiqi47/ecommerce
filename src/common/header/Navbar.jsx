7bb3e972aada6da34e4636496cc88d8a026ec44a
qiqi47 <c756738098@126.com>
2023-11-03 10:16:43 +0800

    ui edit

--- src/common/header/Navbar.jsx
+++ src/common/header/Navbar.jsx
@@ -1,51 +1,128 @@
-import React, { useState } from "react"
-import { Link } from "react-router-dom"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const data = [
+    {
+      cateImg: "./images/category/cat1.png",
+      cateName: "Fashion",
+    },
+    {
+      cateImg: "./images/category/cat2.png",
+      cateName: "Electronic",
+    },
+    {
+      cateImg: "./images/category/cat3.png",
+      cateName: "Cars",
+    },
+    {
+      cateImg: "./images/category/cat4.png",
+      cateName: "Home & Garden",
+    },
+    {
+      cateImg: "./images/category/cat5.png",
+      cateName: "Gifts",
+    },
+    {
+      cateImg: "./images/category/cat6.png",
+      cateName: "Music",
+    },
+    {
+      cateImg: "./images/category/cat7.png",
+      cateName: "Health & Beauty",
+    },
+    {
+      cateImg: "./images/category/cat8.png",
+      cateName: "Pets",
+    },
+    {
+      cateImg: "./images/category/cat9.png",
+      cateName: "Baby Toys",
+    },
+    {
+      cateImg: "./images/category/cat10.png",
+      cateName: "Groceries",
+    },
+    {
+      cateImg: "./images/category/cat11.png",
+      cateName: "Books",
+    },
+  ];
   // Toogle Menu
-  const [MobileMenu, setMobileMenu] = useState(false)
+  const [MobileMenu, setMobileMenu] = useState(false);
+  const [CategoriesTab, setCategoriesTab] = useState(false);
+  const changeCategoriesTab = () => {
+    setCategoriesTab(!CategoriesTab);
+  };
   return (
     <>
-      <header className='header'>
-        <div className='container d_flex'>
-          <div className='catgrories d_flex'>
-            <span class='fa-solid fa-border-all'></span>
+      <header className="header">
+        <div className="container d_flex">
+          <div
+            className="catgrories d_flex"
+            onClick={() => changeCategoriesTab()}
+          >
+            <span class="fa-solid fa-border-all"></span>
             <h4>
-              Categories <i className='fa fa-chevron-down'></i>
+              Categories <i className="fa fa-chevron-down"></i>
             </h4>
           </div>
 
-          <div className='navlink'>
-            <ul className={MobileMenu ? "nav-links-MobileMenu" : "link f_flex capitalize"} onClick={() => setMobileMenu(false)}>
+          <div className="navlink">
+            <ul
+              className={
+                MobileMenu ? "nav-links-MobileMenu" : "link f_flex capitalize"
+              }
+              onClick={() => setMobileMenu(false)}
+            >
               {/*<ul className='link f_flex uppercase {MobileMenu ? "nav-links-MobileMenu" : "nav-links"} onClick={() => setMobileMenu(false)}'>*/}
               <li>
-                <Link to='/'>home</Link>
+                <Link to="/">home</Link>
               </li>
               <li>
-                <Link to='/pages'>pages</Link>
+                <Link to="/pages">pages</Link>
               </li>
               <li>
-                <Link to='/user'>user account</Link>
+                <Link to="/user">user account</Link>
               </li>
               <li>
-                <Link to='/vendor'>vendor account</Link>
+                <Link to="/vendor">vendor account</Link>
               </li>
               <li>
-                <Link to='/track'>track my order</Link>
+                <Link to="/track">track my order</Link>
               </li>
               <li>
-                <Link to='/contact'>contact</Link>
+                <Link to="/contact">contact</Link>
               </li>
             </ul>
 
-            <button className='toggle' onClick={() => setMobileMenu(!MobileMenu)}>
-              {MobileMenu ? <i className='fas fa-times close home-btn'></i> : <i className='fas fa-bars open'></i>}
+            <button
+              className="toggle"
+              onClick={() => setMobileMenu(!MobileMenu)}
+            >
+              {MobileMenu ? (
+                <i className="fas fa-times close home-btn"></i>
+              ) : (
+                <i className="fas fa-bars open"></i>
+              )}
             </button>
           </div>
         </div>
       </header>
+      {CategoriesTab && (
+        <div className="category">
+          {data.map((value, index) => {
+            return (
+              <div className="box f_flex" key={index}>
+                <img src={value.cateImg} alt="" />
+                <span>{value.cateName}</span>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/MainPage/Home.jsx
+++ src/components/MainPage/Home.jsx
@@ -1,19 +1,17 @@
-import React from "react"
-import Categories from "./Categories"
-import "./Home.css"
-import SliderHome from "./Slider"
+import React from "react";
+import "./Home.css";
+import SliderHome from "./Slider";
 
 const Home = () => {
   return (
     <>
-      <section className='home'>
-        <div className='container d_flex'>
-          <Categories />
+      <section className="home">
+        <div className="container d_flex">
           <SliderHome />
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
